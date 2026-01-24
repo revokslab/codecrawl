ac94cab8a158ebdefb161210965d5075f8323efa
irere123 <irere2020@outlook.com>
2026-01-24 10:04:57 +0200

    fix: fetching github stars

--- apps/web/src/routes/(marketing)/_landing.tsx
+++ apps/web/src/routes/(marketing)/_landing.tsx
@@ -1,32 +1,27 @@
 import { Flex } from '@radix-ui/themes'
 import { createFileRoute, Outlet } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/react-start'
+import { useEffect, useState } from 'react'
 
 import { Footer } from '~/components/marketing/footer'
 import { Header } from '~/components/marketing/header'
 
-export const getGithubStars = createServerFn({
-  method: 'GET',
-}).handler(async () => {
-  const response = await fetch('https://api.github.com/repos/Idee8/codecrawl')
-  const data = await response.json()
-  return (data as { stargazers_count: number }).stargazers_count
-})
 
 export const Route = createFileRoute('/(marketing)/_landing')({
   component: RouteComponent,
-  loader: async ({ context }) => {
-    const stars = await getGithubStars()
-    return { stars }
-  },
 })
 
 function RouteComponent() {
-  const state = Route.useLoaderData()
+  const [stars, setStars] = useState(0)
+
+  useEffect(() => {
+    fetch(`https://api.github.com/repos/revokslab/codecrawl`)
+      .then(response => response.json() as Promise<{ stargazers_count: number }>)
+      .then(data => setStars(data.stargazers_count))
+  }, [])
 
   return (
     <Flex direction={'column'} height={'100%'} flexGrow={'1'} style={{ backgroundColor: 'white' }}>
-      <Header stars={state.stars}  />
+      <Header stars={stars}  />
       <div className='custom-container space-y-12 pt-16 min-h-screen flex flex-col justify-between'>
         <main className='flex-1'>
           <Outlet />
